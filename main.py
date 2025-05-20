56cf87a545edd6f7640a7107f5c5a02e697c8295
Alejandro de León <pepedoy1213@gmail.com>
2025-05-20 17:44:42 -0600

    Registro de usuarios

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from app.AWSConnections import AWSConnections
-
-aws = AWSConnections()
-awsSession = aws.getSession()
-
-def saveUserDynamoDB(session, user):
-  dynamodb = session.resource('dynamodb')
-  table = dynamodb.Table('users')
-  response = table.put_item(Item=user)
-  return response
-
-saveUserDynamoDB(awsSession, {"email": "local@local.com", "age": "19", "dpi": "1245155124", "initial_balance": "10000", "name": "Jose Sanchez"})
+ 

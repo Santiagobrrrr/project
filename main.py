e53dc49395915569ec957cf69c7ed5d974e4df20
Alejandro de León <pepedoy1213@gmail.com>
2025-05-21 11:32:49 -0600

    Mensaje

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.AWSConnections import AWSConnections
 from dotenv import load_dotenv
 from decimal import Decimal
+from colorama import Fore, Style
 
 import os
 import uuid
@@ -22,11 +23,11 @@ initialBalance = float(input("Ingrese su saldo inicial: $"))
 
 userId = str(uuid.uuid4())
 
-print("\nRegistro exitoso!")
-print(f"Nombre: {name}")
-print(f"Correo Electrónico: {email}")
-print(f"Saldo inicial: ${initialBalance:.2f}")
-print(f"UUID: {userId}")
+print(f"{Fore.RED}\nRegistro exitoso!{Style.RESET_ALL}")
+print(f"{Fore.YELLOW}Nombre:{Style.RESET_ALL} {name}")
+print(f"{Fore.GREEN}Correo Electrónico:{Style.RESET_ALL} {email}")
+print(f"{Fore.CYAN}Saldo inicial:{Style.RESET_ALL} ${initialBalance:.2f}")
+print(f"{Fore.MAGENTA}UUID:{Style.RESET_ALL} {userId}")
 
 # Datos a diccionario
 user_data = {
@@ -40,11 +41,11 @@ user_data = {
 aws = AWSConnections()
 awsSession = aws.getSession()
 
-def saveUserDynamoDB(session, user):
-    try:
+def saveUserDynamoDB(session, stock):
+    try:    
         dynamodb = session.resource('dynamodb')
-        table = dynamodb.Table('users')  
-        table.put_item(Item=user)
+        table = dynamodb.Table('stocks')  
+        table.put_item(Item=stock)
         print("\nRegistrado correctamente.")
     except Exception as e:
         print("\nError al registrarse", e)

7eb91273fc5ea1675df3899590d61d74d4e4a93f
Alejandro de León <pepedoy1213@gmail.com>
2025-05-20 18:07:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-"" 
+from app.AWSConnections import AWSConnections
+
+aws = AWSConnections()
+awsSession = aws.getSession()
+
+def saveUserDynamoDB(session, user):
+  dynamodb = session.resource('dynamodb')
+  table = dynamodb.Table('users')
+  response = table.put_item(Item=user)
+  return response
+
+saveUserDynamoDB(awsSession, {"email": "local@local.com", "age": "19", "dpi": 
+"1245155124", "initial_balance": "10000", "name": "Jose Sanchez"})
+ 
+ #Registro de usuario
+import uuid
+import time
+
+print("\nBienvenido a tu sistema de inversiones\n")
+print("------------------------------------------\n")
+
+name = input("Ingrese su nombre: ")
+email = input("Ingrese su correo electrónico: ")
+initialBalance = float(input("Ingrese su saldo inicial: $"))
+
+print("\nRegistrando usuario...")
+time.sleep(1.5)
+
+userId = uuid.uuid4()
+
+print("\nRegistro exitoso!")
+print(f"Nombre: {name}")
+print(f"Correo Electrónico: {email}")
+print(f"Saldo inicial: ${initialBalance:.2f}")
+print(f"UUID: {userId}")
